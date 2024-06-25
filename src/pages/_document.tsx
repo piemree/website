9d3ec3e6a14aeeda9fd15d2464f8502b499d9221
emre <pi.emree@gmail.com>
2024-06-25 03:07:36 +0300

    Add lang

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document(ctx: any) {
   return (
-    <Html lang={ctx.locale}>
+    <Html lang="tr">
       <Head>
         <link
           rel="apple-touch-icon"
